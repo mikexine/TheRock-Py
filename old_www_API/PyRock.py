0bc6d68cda41fb0716eef4ad3acee868821a9955
mikexine <mikexine@gmail.com>
2015-07-03 12:51:21 +0200

    new v0 api

---
+++ old_www_API/PyRock.py
@@ -0,0 +1,150 @@
+import json
+import requests
+
+StdUrl = 'https://www.therocktrading.com/api/'
+
+class PyRock:
+    
+      # credentials
+    def __init__(self, Username, Password, ApiKey):
+        self.username=Username
+        self.password=Password
+        self.key = ApiKey
+
+      # get the balance for a certain currency 
+    def GetBalance(self, currency):
+        try:
+            currency = currency.upper()
+            url = StdUrl+'get_balance'
+            values = {
+            'username' : self.username,
+            'password' : self.password,
+            'api_key'  : self.key,
+            'type_of_currency' : currency
+            }
+            headers = {'content-type': 'application/json'}
+            r = requests.post(url, data=json.dumps(values), headers = headers)
+            return r.json()
+        except:
+            return None
+
+      # get discount level for a certain currency
+    def GetDiscountLevel(self, currency):
+        try:
+            currency = currency.upper()
+            url = StdUrl+'get_discountlevel'
+            values = {
+            'username' : self.username,
+            'password' : self.password,
+            'api_key'  : self.key,
+            'type_of_currency' : currency
+            }
+            headers = {'content-type': 'application/json'}
+            r = requests.post(url, data = json.dumps(values), headers = headers)
+            return r.json()
+        except:
+            return None
+
+
+      # get the list of active orders for the account 
+    def GetOrders(self):
+        try:
+            url = StdUrl+'get_orders'
+            values = {
+            'username' : self.username,
+            'password' : self.password,
+            'api_key'  : self.key
+            }
+            headers = {'content-type': 'application/json'}
+            r = requests.post(url, data = json.dumps(values), headers = headers)
+            return r.json()
+        except:
+            return None
+
+      # placing a buy order
+    def PlaceBuyOrder(self, fund, amount, price):
+        try:
+            fund = fund.upper()
+            url = StdUrl+'place_order'
+            values = {
+            'username'   : self.username,
+            'password'   : self.password,
+            'api_key'    : self.key,
+            'fund_name'  : fund,
+            'order_type' : 'B',
+            'amount'     : amount,
+            'price'      : price
+            }
+            headers = {'content-type': 'application/json'}
+            r = requests.post(url, data = json.dumps(values), headers = headers)
+            return r.json()
+        except:
+            return None
+
+      # placing a sell order
+    def PlaceSellOrder(self, fund, amount, price):
+        try:
+            fund = fund.upper()
+            url = StdUrl+'place_order'
+            values = {
+            'username'   : self.username,
+            'password'   : self.password,
+            'api_key'    : self.key,
+            'fund_name'  : fund,
+            'order_type' : 'S',
+            'amount'     : amount,
+            'price'      : price
+            }
+            headers = {'content-type': 'application/json'}
+            r = requests.post(url, data = json.dumps(values), headers = headers)
+            return r.json()
+        except:
+            return None
+
+      # cancel a specified order
+    def CancelOrder(self, orderid):
+        try:
+            url = StdUrl+'cancel_order'
+            values = {
+            'username'   : self.username,
+            'password'   : self.password,
+            'api_key'    : self.key,
+            'order_id'   : orderid
+            }
+            headers = {'content-type': 'application/json'}
+            r = requests.post(url, data = json.dumps(values), headers = headers)
+            return r.json()
+        except:
+            return None
+    
+      # getting ticker for a certain currency / fund
+    def MarketData(self, fund):
+        self.fund = fund.upper()
+        url = StdUrl+"ticker/"+self.fund
+        headers = {'content-type': 'application/json'}
+        r = requests.get(url, headers = headers)
+        return r.json()
+
+      # get all market tickers
+    def AllMarketData(self):
+        url = StdUrl+"tickers/"
+        headers = {'content-type': 'application/json'}
+        r = requests.get(url, headers = headers)
+        return r.json()
+
+      # getting last trades for a fund since a date
+    def LastTrades(self, fund, date):
+        self.date = date
+        self.fund = fund.upper()
+        url = StdUrl+"trades/"+self.fund+"/?since="+self.date
+        headers = {'content-type': 'application/json'}
+        r = requests.get(url, headers = headers)
+        return r.json()
+
+      # getting the full orderbook
+    def OrderBook(self, fund):
+        self.fund=fund.upper()
+        url = StdUrl+"orderbook/"+self.fund
+        headers = {'content-type': 'application/json'}
+        r = requests.get(url, headers = headers)
+        return r.json()
---
+++ old_www_API/example.py
@@ -0,0 +1,88 @@
+from PyRock import PyRock
+from time import sleep
+
+## this is just an example. if you want to run it, you need to insert
+## your username, password and API key - you can generate one in your 
+## personal page on The Rock.
+##
+## Note that MarketData, AllMarketData, LastTrades and GetBalance functions
+## do NOT require an API KEY 
+##
+## I put a sleep(1) between each query because The Rock has a request limit
+## which is five API calls per second. This is just an example, so there's
+## no need to be fast. 
+##
+## You should have a look on the code before putting in it your account
+## data and running it. This one creates also some orders, so you might
+## check them before they are created. Also, there might be some bug or
+## error, so use this with caution.
+##
+## Do whatever you want with this code - but keep in mind that I do NOT
+## take any responsibility for errors or losses caused by this script.
+
+
+rock = PyRock('USERNAME','PASSWORD', 'API_KEY')
+
+## asking TheRock for the data of the fund 'btceur' 
+## (this is BTC/EUR, it may be any other listed on TRT)
+Data = rock.MarketData('btceur')
+print(Data)
+print('')
+sleep(1)
+
+## asking TheRock for all market tickers
+AllData = rock.AllMarketData()
+print(AllData)
+print('')
+sleep(1)
+
+## asking the rock for the trades of the 'eurdog' fund since 1400284800
+## (this is EUR/DOGE trades, since 1400284800; date expressed in UnixTime)
+Trades = rock.LastTrades('eurdog', '1400284800')
+print(Trades)
+print('')
+sleep(1)
+
+## asking the rock for the OrderBook of the 'btcusd' fund
+## (this is BTC/USD, it provides both bids and asks)
+FullOrderBook = rock.OrderBook('btcusd')
+print(FullOrderBook)
+print('')
+sleep(1)
+
+## asking the rock for my available balance of a specified currency
+## in this case the currency is Euro
+MyBalance = rock.GetBalance('eur')
+print(MyBalance)
+print('')
+sleep(1)
+
+## asking the rock for all my open orders
+MyOrders = rock.GetOrders()
+print(MyOrders)
+print('')
+sleep(1)
+
+## placing a buy order for 1 euro at 2 doge
+PlaceBuy = rock.PlaceBuyOrder('eurdog', '1', '2')
+print(PlaceBuy)
+print('')
+sleep(1)
+
+## placing a sell order for 1 btc at 500 eur
+PlaceSell = rock.PlaceSellOrder('btceur', '1', '500')
+print(PlaceSell)
+print('')
+sleep(1)
+
+## cancelling an order with the specified order_id
+CancelIt = rock.CancelOrder('16657582')
+print(CancelIt)
+print('')
+sleep(1)
+
+## getting the Discount Level for Euro
+Discount = rock.GetDiscountLevel('eur')
+print(Discount)
+print('')
+sleep(1)
